8d1050c1eaca70afdebf8b69039f3d0988bd1080
gaphodil <gaphodillo@gmail.com>
2024-02-06 21:43:04 -0800

    update navbar and home; add theme changer

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,10 +1,11 @@
 <template>
-  <v-container>
+  <v-container style="padding-top: 0px">
     <v-row
       align="center"
       justify="center"
+      justify-sm="start"
     >
-      <v-col :cols="'auto'">
+      <NavBarItem>
         <v-btn
           flat
           :href="tabUrls[0]"
@@ -13,8 +14,8 @@
         >
           Home
         </v-btn>
-      </v-col>
-      <v-col :cols="'auto'">
+      </NavBarItem>
+      <NavBarItem>
         <v-btn
           flat
           :href="tabUrls[1]"
@@ -23,8 +24,8 @@
         >
           About
         </v-btn>
-      </v-col>
-      <v-col :cols="'auto'">
+      </NavBarItem>
+      <NavBarItem>
         <v-btn
           flat
           :href="tabUrls[2]"
@@ -33,8 +34,8 @@
         >
           Projects
         </v-btn>
-      </v-col>
-      <v-col :cols="'auto'">
+      </NavBarItem>
+      <NavBarItem>
         <v-btn
           flat
           :href="tabUrls[3]"
@@ -43,11 +44,59 @@
         >
           Speedrunning
         </v-btn>
+      </NavBarItem>
+      <v-spacer />
+      <v-col
+        cols="12"
+        sm="1"
+        :class="$vuetify.display.mobile ? 'd-flex justify-center' : ''"
+        :style="$vuetify.display.mobile ? 'padding: 0px' : ''"
+      >
+        <v-btn
+          style="justify-self: center;"
+          aria-label="Change Theme"
+          title="Change Theme"
+          :icon="$vuetify.theme.current.dark ? 'mdi-weather-night' : 'mdi-weather-sunny'"
+          @click="toggleTheme"
+        />
       </v-col>
     </v-row>
   </v-container>
 </template>
 
+<script setup>
+import { useTheme } from 'vuetify'
+
+const theme = useTheme()
+
+// read localStorage for theme
+if (localStorage.getItem('theme')) {
+  theme.global.name.value = localStorage.getItem('theme')
+}
+// otherwise, on first visit
+else {
+  // grab system theme from window.matchMedia
+  if (window.matchMedia) {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      theme.global.name.value = 'dark'
+    }
+    else {
+      theme.global.name.value = 'light'
+    }
+  }
+  // if browser too old, default to dark
+  else {
+    theme.global.name.value = 'dark'
+  }
+  // write to localStorage
+  localStorage.setItem('theme', theme.global.name.value)
+}
+
+function toggleTheme () {
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+  localStorage.setItem('theme', theme.global.name.value)
+}
+</script>
 
 <script>
 var activePage = window.location.pathname;
---
+++ src/components/NavBarItem.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-col
+    cols="12"
+    sm="auto"
+    :class="$vuetify.display.mobile ? 'd-flex justify-center' : ''"
+    :style="$vuetify.display.mobile ? 'padding: 2px' : ''"
+  >
+    <slot />
+  </v-col>
+</template>
